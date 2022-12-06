075c0eeb949efe9494d1247be18c817231573d27
Oliver Oxenham <oliver.oxenham@gmail.com>
2022-12-06 16:44:39 +0400

    removed auth from index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,17 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { ...pageProps },
 }) => {
   return (
     <>
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-      <SessionProvider session={session}>
         <Component {...pageProps} />
-      </SessionProvider>
     </>
   );
 };
