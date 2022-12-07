a4af63577a30f71c460c0c370537448dd58e011b
Oliver Oxenham <oliver.oxenham@gmail.com>
2022-12-07 12:35:12 +0400

    refined background gradient

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <Link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
-      <body className="bg-gradient-to-r from-[#355C7D] via-[#223d6a] to-[#003343] background-animate">
+      <body className="bg-gradient-to-r from-[#00203a] via-[#182945] to-[#003343] background-animate">
         <Main />
         <NextScript />
       </body>
