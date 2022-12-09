de6980a5f368e005c4c9777548f4c3dbec37d7fe
Oliver Oxenham <oliver.oxenham@gmail.com>
2022-12-09 12:13:21 +0400

    added guestbook page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import {
 } from "@heroicons/react/24/solid";
 import Logo from "../components/Logo/Logo";
 
-const MenuLink = ({ href, target = "_blank", children }: any) => {
+const MenuLink = ({ href, target = "_self", children }: any) => {
   return (
     <Link
       className="flex max-w-xs flex-col gap-5 rounded-xl bg-white/10 p-5 text-white hover:bg-white/20"
@@ -28,17 +28,19 @@ const Home: NextPage = () => {
       <Head>
         <title>Oliverox.Dev</title>
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center">
+      <main className="flex flex-col items-center justify-center min-h-screen">
         <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16 ">
           <Logo />
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[4em]">
             OliverOx<span className="text-[#4ee4fc]">.dev</span>
           </h1>
           <span className="text-center text-lg text-white">
-            Your web development needs<br />under control with Oliver
+            Your web development needs
+            <br />
+            under control with Oliver
           </span>
           <div className="mt-10 grid grid-cols-1 gap-5 sm:grid-cols-2 md:gap-8">
-            <MenuLink href="">
+            <MenuLink href="/foundation">
               <div className="fx-row flex items-center space-x-2">
                 <HeartIcon className="h-6 w-6" />
                 <h3 className="text-2xl font-bold">Foundation →</h3>
@@ -48,7 +50,7 @@ const Home: NextPage = () => {
                 web development needs.
               </div>
             </MenuLink>
-            <MenuLink href="https://create.t3.gg/en/introduction">
+            <MenuLink href="/projects">
               <div className="fx-row flex items-center space-x-2">
                 <CodeBracketSquareIcon className="h-6 w-6" />
                 <h3 className="text-2xl font-bold">Projects →</h3>
@@ -58,7 +60,7 @@ const Home: NextPage = () => {
                 is currently building.
               </div>
             </MenuLink>
-            <MenuLink href="https://create.t3.gg/en/usage/first-steps">
+            <MenuLink href="/techstack">
               <div className="flex flex-row items-center space-x-2">
                 <Square3Stack3DIcon className="h-6 w-6" />
                 <h3 className="text-2xl font-bold">Tech Stack →</h3>
@@ -68,7 +70,7 @@ const Home: NextPage = () => {
                 building high quality web apps.
               </div>
             </MenuLink>
-            <MenuLink href="https://create.t3.gg/en/usage/first-steps">
+            <MenuLink href="/guestbook">
               <div className="flex flex-row items-center space-x-2">
                 <BookOpenIcon className="h-6 w-6" />
                 <h3 className="text-2xl font-bold">Guestbook →</h3>
@@ -80,43 +82,41 @@ const Home: NextPage = () => {
           </div>
         </div>
       </main>
-      <footer>
-        <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
-          <div className="flex justify-center space-x-6 md:order-2">
-            <a
-              href="https://twitter.com/oliveroxenham"
-              target="_blank"
-              rel="noreferrer"
-              className="text-gray-400 hover:text-gray-500"
+      <footer className="mx-auto max-w-7xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center space-x-6 md:order-2">
+          <a
+            href="https://twitter.com/oliveroxenham"
+            target="_blank"
+            rel="noreferrer"
+            className="text-gray-400 hover:text-gray-500"
+          >
+            <svg
+              className="h-5 w-5 fill-gray-400 hover:fill-white"
+              viewBox="0 0 24 24"
             >
-              <svg
-                className="h-5 w-5 fill-gray-400 hover:fill-white"
-                viewBox="0 0 24 24"
-              >
-                <title>Twitter</title>
-                <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
-              </svg>
-            </a>
-            <a
-              href="https://mu.linkedin.com/in/oliver-oxenham-a33696"
-              target="_blank"
-              rel="noreferrer"
-              className="text-gray-400 hover:text-gray-500"
+              <title>Twitter</title>
+              <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
+            </svg>
+          </a>
+          <a
+            href="https://mu.linkedin.com/in/oliver-oxenham-a33696"
+            target="_blank"
+            rel="noreferrer"
+            className="text-gray-400 hover:text-gray-500"
+          >
+            <svg
+              className="h-5 w-5 fill-gray-400 hover:fill-white"
+              viewBox="0 0 24 24"
             >
-              <svg
-                className="h-5 w-5 fill-gray-400 hover:fill-white"
-                viewBox="0 0 24 24"
-              >
-                <title>LinkedIn</title>
-                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
-              </svg>
-            </a>
-          </div>
-          <div className="mt-8 md:order-1 md:mt-0">
-            <p className="text-center text-sm text-gray-400">
-              &copy; 2022 Oliver Oxenham. All rights reserved.
-            </p>
-          </div>
+              <title>LinkedIn</title>
+              <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+            </svg>
+          </a>
+        </div>
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className="text-center text-sm text-gray-400">
+            &copy; 2022 Oliver Oxenham. All rights reserved.
+          </p>
         </div>
       </footer>
     </>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        'neutral': '#3d4451',
-        'primary': '#570df8',
-        'accent': '#4ee4fc',
-        'info': '#3abff8',
-        'warning': '#fbbd23',
-        'success': '#36d399',
-        'error': '#f87272'
-      },
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      }
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
