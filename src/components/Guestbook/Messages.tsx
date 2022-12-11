25da781e57b955741852dc05c432ef6c7b285bf6
Oliver Oxenham <oliver.oxenham@gmail.com>
2022-12-11 15:55:04 +0400

    fixed chat bubbles text alignment

--- src/components/Guestbook/Messages.tsx
+++ src/components/Guestbook/Messages.tsx
@@ -23,9 +23,11 @@ export default function Messages() {
       {messages?.map((msg: msgType, index: number) => {
         const chatContainerClasses = ["chat", "basis-full"];
         const chatBubbleClasses = ["chat-bubble", "mx-auto", "p-4", "w-5/6"];
+        const chatTextClasses = ["mt-2", "flex", "flex-col"];
         if (msg.name === "Oliver Oxenham") {
           chatContainerClasses.push("chat-end");
           chatBubbleClasses.push("chat-bubble-neutral");
+          chatTextClasses.push("text-right");
         } else {
           chatBubbleClasses.push("chat-bubble-accent");
         }
@@ -33,9 +35,12 @@ export default function Messages() {
           <div key={index} className={chatContainerClasses.join(" ")}>
             <div className={chatBubbleClasses.join(" ")}>
               <p>{msg.message}</p>
-              <span className="text-[11px] text-gray-400">
-                {msg.name} &#8226; {msg.createdAt.toDateString()}
-              </span>
+              <div className={chatTextClasses.join(" ")}>
+                <span className="text-[11px] italic">{msg.name}</span>
+                <span className="text-[11px] text-gray-400">
+                  {msg.createdAt.toDateString()}
+                </span>
+              </div>
             </div>
           </div>
         );
